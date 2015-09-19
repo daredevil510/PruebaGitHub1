b9b905b5e8b6ad2188c4c990a80e730e6b3a5a20
daredevil510 <emmanuel.otaku@hotmail.com>
2015-09-18 18:51:24 -0600

    Define variable id of type int.

---
+++ PruebaGitHub1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGitHub1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            float num;
+
+        }
+    }
+}
